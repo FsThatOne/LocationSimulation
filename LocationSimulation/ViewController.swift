e3da873d268e7404134293762c027f2acc1acd2c
王正一 <fsymbio@icloud.com>
2017-06-02 07:40:14 +0800

    迁移

---
+++ LocationSimulation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationSimulation
+//
+//  Created by 王正一 on 2017/4/20.
+//  Copyright © 2017年 FsThatOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
